6f076d826623ee5263bf04041a678e61158ac76e
Jay Tyrrell <tyrrell.jay@gmail.com>
2024-12-06 10:47:34 -0500

    refactor: remove unused string input

--- pkg/ui/inputs.go
+++ pkg/ui/inputs.go
@@ -1,28 +1,10 @@
 package ui
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
 	"strings"
 
 	"github.com/charmbracelet/huh"
 )
-
-func StringPrompt(label string, stdin io.Reader) string {
-	var s string
-	r := bufio.NewReader(stdin)
-	for {
-		fmt.Fprint(os.Stderr, label+" ")
-		s, _ = r.ReadString('\n')
-		if s != "" {
-			break
-		}
-	}
-
-	return strings.TrimSpace(s)
-}
 
 func Input(title string, placeholder string) (string, error) {
 	var s string
